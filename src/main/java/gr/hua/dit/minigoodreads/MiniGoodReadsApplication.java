f9d4811325da60b27bb76d54e4e1ab2780bea3be
petros-voivontas <petrosvoivontas@gmail.com>
2022-12-29 17:08:19 +0200

    Initial commit

---
+++ src/main/java/gr/hua/dit/minigoodreads/MiniGoodReadsApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.minigoodreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGoodReadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniGoodReadsApplication.class, args);
+    }
+
+}
