393159c093c25e0e7d6cb3013ac576eaa49bd0eb
petros-voivontas <petrosvoivontas@gmail.com>
2023-01-08 17:10:10 +0200

    Separate the Configuration for LocalValidator and MessageSource

--- src/main/java/gr/hua/dit/minigoodreads/config/LocalValidatorConfig.java
+++ src/main/java/gr/hua/dit/minigoodreads/config/LocalValidatorConfig.java
@@ -3,24 +3,21 @@ package gr.hua.dit.minigoodreads.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class LocalValidatorConfig {
 
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
+    private final MessageSource messageSource;
+
+    public LocalValidatorConfig(MessageSource messageSource) {
+        this.messageSource = messageSource;
     }
 
     @Bean
     public LocalValidatorFactoryBean getValidator() {
         LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
-        validatorFactoryBean.setValidationMessageSource(messageSource());
+        validatorFactoryBean.setValidationMessageSource(messageSource);
         return validatorFactoryBean;
     }
 }
