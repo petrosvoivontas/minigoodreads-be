5d3b050e83f4f06c5639d1b6546889dbc6e7f044
petros-voivontas <petrosvoivontas@gmail.com>
2023-07-15 19:41:46 +0300

    Remove keycloak from sql scripts

--- docker/mysql/init/01-users.sql
+++ docker/mysql/init/01-users.sql
@@ -1,13 +1,9 @@
 CREATE
 USER 'root'@'%' IDENTIFIED by 'root';
 CREATE
-USER 'keycloak'@'%' IDENTIFIED by 'keycloak';
-CREATE
 USER 'minigoodreads'@'%' IDENTIFIED BY 'minigoodreads';
 
 GRANT ALL PRIVILEGES ON *.* TO
 'root'@'%';
-GRANT ALL PRIVILEGES ON *.* TO
-'keycloak'@'%';
 GRANT ALL PRIVILEGES ON *.* TO
 'minigoodreads'@'%';
