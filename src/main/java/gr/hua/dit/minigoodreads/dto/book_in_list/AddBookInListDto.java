d312d1fe32928b55d37195723e237d0940c8642e
petros-voivontas <petrosvoivontas@gmail.com>
2023-07-19 19:51:01 +0300

    Change spring boot version to 2.7.5

--- src/main/java/gr/hua/dit/minigoodreads/dto/book_in_list/AddBookInListDto.java
+++ src/main/java/gr/hua/dit/minigoodreads/dto/book_in_list/AddBookInListDto.java
@@ -1,19 +1,20 @@
 package gr.hua.dit.minigoodreads.dto.book_in_list;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
+
+import javax.validation.constraints.NotBlank;
 
 public record AddBookInListDto(
-    @NotBlank(message = "{book_in_list.add.book_id.empty}")
-    @JsonProperty("bookId")
-    String bookId,
-    @JsonProperty("coverImageUrl")
-    String coverImageUrl,
-    @NotBlank(message = "{book_in_list.add.book_title.empty}")
-    @JsonProperty("bookTitle")
-    String bookTitle,
-    @NotBlank(message = "{book_in_list.add.book_author.empty}")
-    @JsonProperty("bookAuthor")
-    String bookAuthor
+	@NotBlank(message = "{book_in_list.add.book_id.empty}")
+	@JsonProperty("bookId")
+	String bookId,
+	@JsonProperty("coverImageUrl")
+	String coverImageUrl,
+	@NotBlank(message = "{book_in_list.add.book_title.empty}")
+	@JsonProperty("bookTitle")
+	String bookTitle,
+	@NotBlank(message = "{book_in_list.add.book_author.empty}")
+	@JsonProperty("bookAuthor")
+	String bookAuthor
 ) {
 }
