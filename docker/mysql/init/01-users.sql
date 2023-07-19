b8c3cbca159e1baf361a5234abbc5058706a7ad9
petros-voivontas <petrosvoivontas@gmail.com>
2023-07-19 19:53:49 +0300

    Init sql scripts for mysql

--- docker/mysql/init/01-users.sql
+++ docker/mysql/init/01-users.sql
@@ -1,9 +1,20 @@
 CREATE
-USER 'root'@'%' IDENTIFIED by 'root';
-CREATE
-USER 'minigoodreads'@'%' IDENTIFIED BY 'minigoodreads';
+    USER 'minigoodreads'@'%' IDENTIFIED BY 'minigoodreads';
 
 GRANT ALL PRIVILEGES ON *.* TO
-'root'@'%';
-GRANT ALL PRIVILEGES ON *.* TO
-'minigoodreads'@'%';
+    'minigoodreads'@'%';
+
+CREATE TABLE users
+(
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(100) not null,
+    enabled  boolean      not null
+);
+
+CREATE TABLE IF NOT EXISTS authorities
+(
+    username  varchar(50) NOT NULL,
+    authority varchar(50) NOT NULL,
+    unique (username, authority),
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
+);
