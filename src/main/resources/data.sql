d32c62221e4fcf8a94f5871d774637c0f2d5c7a2
petros-voivontas <petrosvoivontas@gmail.com>
2023-07-22 12:34:19 +0300

    Fix database initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO BOOKS_LIST (resource_id, list_id, uid, name)
+INSERT INTO books_list (resource_id, list_id, uid, name)
 VALUES ('19c6aca9-9101-49ae-8b27-8f93127af4af', 0, null, 'Want to read'),
        ('edf4977d-1eb2-475c-8a2c-af6243d94c3f', 1, null, 'Currently reading'),
        ('9c83c19c-8543-4669-a8d3-ded55ca07710', 2, null, 'Read');
